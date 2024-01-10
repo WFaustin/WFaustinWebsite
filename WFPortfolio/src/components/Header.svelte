b2a9aae9514cc1d75d713bdd4d786ccda0f5bdee
Widchard Faustin <widchardfaustin@waxwing.studio>
2024-01-10 00:31:58 -0500

    First update of the svelte personal site. At time stamp 32:45 of video. Finished the main section, need to complete about me, projects, contact, and the wfrecordings link. Good progress!

---
+++ WFPortfolio/src/components/Header.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let y;
+
+    let tabs = [
+        {name: 'About Me', link: '/about'},
+        {name: 'Technical Projects', link: '/projects'},
+    ]
+    let finaltab = {name: 'Contact', link: '#contact'};
+</script>
+
+<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid'
++ (y > 0 ? 'py-4 bg-slate-950 border-violet-950' : 'py-6 bg-transparent border-transparent')}
+>
+    <h1 class="font medium">
+        <a href="/" class='duration-299 hover:text-violet-400'>
+            <b class='font-bold poppins'>Widchard Faustin</b>
+        </a>
+    </h1>
+    <div data-sveltekit-preload-data="false" class="sm:flex items-center gap-4 hidden">
+        {#each tabs as tab, index}
+            <a href={tab.link} class="duration-299 hover:text-violet-400">
+                <p>{tab.name}</p>
+            </a>
+        {/each}
+    </div>
+    <a href={finaltab.link}
+    class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white
+    text-slate-950">
+        <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20
+        group-hover:translate -x-full z-0 duration-200">
+
+        </div>
+        <h4 class="relative z-9">{finaltab.name} &rarr;</h4>
+    </a>
+</header>
---
+++ WFPortfolio/src/components/Main.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:text-center gap-6 md:gap-8
+        lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                <span class="poppins text-violet-400"> Widchard </span> Faustin
+            </h2>
+            <h3 class="font-semibold text-2xl sm:text-3xl md:text-3xl">
+                <br/> Full Stack <span class="poppins text-violet-400">Developer</span>
+                <br/> Game <span class="poppins text-violet-400">Programmer</span>
+                <br/> Musician | <span class="poppins text-violet-400">Composer</span>
+                <br/> Audio <span class="poppins text-violet-400">Engineer</span>
+            </h3>
+            <p class="text-base sm:text-lg md:text-xl">
+                Some of my <span class="text-violet-400">favorite</span> languages/frameworks are Python, C#, Node.JS, SvelteJS, and Godot.
+            </p>
+        </div>
+        <div class="relative shadow-2xl grid place-items-center opacity-100">
+            <img src={"assets/WidchardWeb.png"} alt="Profile Image" class="object-cover z-[2] max-w-[45vw] max-h-[70vh]"/>
+        </div>
+    </section>
+</main>
+
